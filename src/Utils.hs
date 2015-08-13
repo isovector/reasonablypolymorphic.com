9924c7b5eadbb7c6b3061721e4f7162104003daa
Sandy Maguire <sandy@sandymaguire.me>
2015-08-13 12:38:32 +0300

    cleaned up clips

--- src/Utils.hs
+++ src/Utils.hs
@@ -6,7 +6,6 @@ import Data.Map (fromListWith, toList)
 sortAndGroup :: Ord k => (a -> k) -> [a] -> [[a]]
 sortAndGroup key as = map snd
                     . toList
-                    $ fromListWith
-                        (++)
-                        [(k, [v]) | (k, v) <- zip =<< map key $ as]
+                    $ fromListWith (++)
+                        [(key a, [a]) | a <- as]
 
