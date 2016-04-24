8b202826900e6db9399b381729e22dae2a1d4591
Sandy Maguire <sandy@sandymaguire.me>
2016-04-24 01:44:42 -0700

    update deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ if [ "$(whoami)" ==  "ubuntu" ]; then
     cd /data/blog
     git pull origin master
     stack install
-    rm -r _live/*
+    rm -r _live/we-can-solve-this _live/reasonably-polymorphic
     /data/local/bin/wcst clean
     /data/local/bin/wcst build
-    cp -r _site _live
+    cp -r _site _live/we-can-solve-this
     /data/local/bin/poly clean
     /data/local/bin/poly build
-    cp -r _site _live
+    cp -r _site _live/reasonably-polymorphic
 
 else
     git push
