464ac8293895a22c9ad936d9ba825219553ec3cf
Sandy Maguire <sandy@sandymaguire.me>
2016-11-01 05:08:37 -0700

    cleanup

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,8 +7,24 @@ echo -n "New slug: "
 read slug
 echo -n "Blog: "
 read blog
+
+case "$blog" in
+  wcst)
+    loc="we-can-solve-this/posts"
+    ;;
+  poly)
+    loc="reasonably-polymorphic/posts"
+    ;;
+  httw)
+    loc="reasonably-polymorphic/httw"
+    ;;
+  *)
+    return
+    ;;
+esac
+
 WIP="wip/${wipslug}.markdown"
-SLUG="${blog}/posts/$(date +'%Y-%m-%d')-${slug}.markdown"
+SLUG="${loc}/$(date +'%Y-%m-%d')-${slug}.markdown"
 git mv $WIP $SLUG
 sed -i "s/TO_BE_DETERMINED/$(date +'%Y-%m-%d %H:%M')/" $SLUG
 git commit -m "published ${slug}"
