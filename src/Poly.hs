88727b3c19e4724f31d6fea644ad5f5daba5752a
Sandy Maguire <sandy@sandymaguire.me>
2017-07-29 13:22:28 -0600

    don't build the book every damn time

--- src/Poly.hs
+++ src/Poly.hs
@@ -22,9 +22,9 @@ main :: IO ()
 main = do
     hakyll $ do
         tags     <- buildTags (postsDir poly) (fromCapture . fromGlob $ poly ++ "tags/*.html")
-        booktags <- buildTags (bookDir poly) (fromCapture . fromGlob $ poly ++ "book/tags/*.html")
+        -- booktags <- buildTags (bookDir poly) (fromCapture . fromGlob $ poly ++ "book/tags/*.html")
         let postCtxTags = postCtxWithTags tags
-            bookCtxTags = postCtxWithTags booktags
+            -- bookCtxTags = postCtxWithTags booktags
         templateRules poly
         imageRules    poly
         staticRules   poly
@@ -32,11 +32,11 @@ main = do
         cssRules      poly
         postRules     poly postsDir "posts/" "blog/" postCtxTags
         archiveRules  poly postsDir "blog/archives/" postCtxTags
-        postRules     poly bookDir "httw/" "book/" bookCtxTags
+        -- postRules     poly bookDir "httw/" "book/" bookCtxTags
         -- archiveRules  poly bookDir "book/" bookCtxTags
         indexRules    poly bookDir "book/" postCtxTags
         indexRules    poly postsDir "" postCtxTags
         feedRules     poly feedConfiguration
         tagRules      poly tags
-        tagRules      poly booktags
+        -- tagRules      poly booktags
 
