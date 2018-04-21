4eea08280497d4d69ff2c68a8b0ef489d2a58079
Sandy Maguire <sandy@sandymaguire.me>
2018-04-21 19:23:03 +0300

    site pipeify

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,16 +5,16 @@ echo -n "Post slug: "
 read wipslug
 echo -n "New slug: "
 read slug
-echo -n "Blog: "
 
-loc="reasonably-polymorphic/posts"
+loc="site/posts"
 
 WIP="wip/${wipslug}.markdown"
 SLUG="${loc}/$(date +'%Y-%m-%d')-${slug}.markdown"
 git mv $WIP $SLUG || mv $WIP $SLUG
 sed -i "s/TO_BE_DETERMINED/$(date +'%Y-%m-%d %H:%M')/" $SLUG
-git add -u
+git add $SLUG
 git commit -m "published ${slug}"
 
 echo "Published as $SLUG"
+
 
