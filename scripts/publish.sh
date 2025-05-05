76d80b3dc916db615ce523761d298a430c115e0f
Sandy Maguire <sandy@sandymaguire.me>
2025-05-05 07:32:14 -0700

    automatic deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ls wip
 echo -n "Post slug: "
@@ -10,10 +10,8 @@ loc="site/posts"
 
 WIP="wip/${wipslug}.markdown"
 SLUG="${loc}/$(date +'%Y-%m-%d')-${slug}.markdown"
-git mv $WIP $SLUG || mv $WIP $SLUG
+mv $WIP $SLUG || mv $WIP $SLUG
 sed -i "s/TO_BE_DETERMINED/$(date +'%Y-%m-%d %H:%M')/" $SLUG
-git add $SLUG
-git commit -m "published ${slug}"
 
 echo "Published as $SLUG"
 
